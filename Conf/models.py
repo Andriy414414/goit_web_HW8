400afe3d3e76cac953758e30a6f3c83fe7689dc8
Andriy414414 <133632261+Andriy414414@users.noreply.github.com>
2023-11-11 23:05:49 +0200

    5

--- Conf/models.py
+++ Conf/models.py
@@ -7,6 +7,11 @@ class Author(Document):
     born_date = StringField(max_length=50)
     born_location = StringField(max_length=150)
     description = StringField()
-    meta = {"collection": "author"}
+    meta = {"collection": "authors"}
 
-class Author
+
+class Quote(Document):
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE)
+    tags = ListField(StringField(max_length=15))
+    quote = StringField()
+    meta = {"collection": "quotes"}
