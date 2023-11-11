6e784e708724593d7633834d4275a2599c152ca4
Andriy414414 <133632261+Andriy414414@users.noreply.github.com>
2023-11-11 21:56:28 +0200

    2

---
+++ Conf/db.py
@@ -0,0 +1,17 @@
+import configparser
+import pathlib
+from mongoengine import connect
+
+file_config = pathlib.Path(__file__).parent.parent.joinpath('config.ini')
+config = configparser.ConfigParser()
+config.read(file_config)
+
+username = config.get('DB', 'USER')
+password = config.get('DB', 'PASSWORD')
+db_name = config.get('DB', 'DB_NAME')
+domain = config.get('DB', 'DOMAIN')
+retry_writes = config.get('DB', 'RETRY_WRITES')
+ssl = config.get('DB', 'SSL')
+
+
+connect(host=f"""mongodb+srv://{username}:{password}@{domain}/{db_name}?retryWrites=true&w=majority""", ssl=True)
